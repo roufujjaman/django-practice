76abb54798daf25b0467e1f3a3791975cbe25627
Roufujjaman <rrhtcode@gmail.com>
2024-02-09 23:20:02 +0600

    f c

---
+++ project_1/app_1/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+import datetime
+
+# Create your views here.
+def home(request):
+    now = datetime.datetime.now()
+    return render(request, 'app_1/body.html', {
+        "newyear": True
+    })
