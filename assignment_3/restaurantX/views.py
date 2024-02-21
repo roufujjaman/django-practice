c86342363dfbccfea969801e1142585f08e8db35
Roufujjaman <rrhtcode@gmail.com>
2024-02-22 05:29:33 +0600

    22 FEB 24

--- assignment_3/restaurantX/views.py
+++ assignment_3/restaurantX/views.py
@@ -3,7 +3,3 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'restaurantX\home.html')
-
-
-def meals(request):
-    return render(request, 'meals/meals.html')
