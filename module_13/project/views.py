19677581e9d73bf18c835b833297b233d10036e0
Roufujjaman <rrhtcode@gmail.com>
2024-02-28 22:03:59 +0600

    28 FEB 24

--- module_13/project/views.py
+++ module_13/project/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from django import forms
 from django.http import HttpResponseRedirect
 
-class NewUser(forms.Form):
-    name = forms.CharField(label="User Name", empty_value="Name")
-    email = forms.EmailField(label="Email")
-    age = forms.IntegerField(label="Age")
-    weight = forms.FloatField(label="Weight")
+# class NewUser(forms.Form):
+#     name = forms.CharField(label="User Name", empty_value="Name")
+#     email = forms.EmailField(label="Email")
+#     age = forms.IntegerField(label="Age")
+#     weight = forms.FloatField(label="Weight")
+#     SIZE_CHOICES = [("S", "Small"), ("L", "Large"), ("XL", "Extra Large")]
+#     size = forms.ChoiceField(label="Size", choices=SIZE_CHOICES)
 
 all_users = []
 
@@ -14,26 +16,16 @@ def home(request):
     return render(request, 'project/index.html')
 
 def form(request):
-    if request.method == 'POST':
-        user = NewUser(request.POST)
-        if user.is_valid():
-            print(type(user))
-            print(type(user.cleaned_data))
+    if request.method == "POST":
+        file = request.FILES["file"]
+        with open('./upload/' + file.name, 'wb+') as destination:
+            for chunk in file.chunks():
+                destination.write(chunk)
+        print(file)
+        return render(request, "project/form.html")
+    
 
-            name = user.cleaned_data['name']
-            email = user.cleaned_data['email']
-            all_users.append({
-                'name': name,
-                'email': email
-                })
-            return HttpResponseRedirect('/users')
-        else:
-            return render(request, 'project/form.html', {
-                "form": user
-            })
-    return render(request, 'project/form.html', {
-        "form": NewUser()
-    })
+    return render(request, "project/form.html")
 
 def users(request):
     return render(request, 'project/users.html', {
