9b4cc40efb9dee9ea638c5c0eedc1b0a5f0d59a9
Roufujjaman <rrhtcode@gmail.com>
2024-03-23 23:20:34 +0600

    23 MAR 24

--- module_14/first_app/models.py
+++ module_14/first_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Student(models.Model):
     roll = models.IntegerField(
-        primary_key=True
+        primary_key=True,
     )
     name = models.CharField(
         max_length=20,
