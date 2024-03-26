833ee82d55fdcae87ab2d8739f7fcba513c1bbfb
Roufujjaman <rrhtcode@gmail.com>
2024-03-26 23:33:57 +0600

    26 MAR 24

--- module_14/first_app/views.py
+++ module_14/first_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-
+from . import models
 # Create your views here.
 def home(request):
-    return render(request, "project/layout.html", {
-        "name": "Roufujjaman"
+    return render(request, "project/students.html", {
+        "students": models.Student.objects.all()
     })
