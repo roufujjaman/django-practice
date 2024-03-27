11772fc34d9d7860dd827c26239f361babb49196
Roufujjaman <rrhtcode@gmail.com>
2024-03-27 22:32:26 +0600

    27 MAR 24

--- module_14/first_app/urls.py
+++ module_14/first_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = "first_app"
 urlpatterns=[
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("delete/<int:roll>", views.delete_student, name="delete_student")
 ]
--- module_14/first_app/views.py
+++ module_14/first_app/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import models
 # Create your views here.
 def home(request):
     return render(request, "project/students.html", {
         "students": models.Student.objects.all()
     })
+
+def delete_student(request, roll):
+    std = models.Student.objects.get(pk = roll).delete()
+    return redirect("first_app:home")
