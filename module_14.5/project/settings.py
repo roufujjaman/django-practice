60f9ff89652fb82eb054bc0dd437d24c598e7e7f
Roufujjaman <rrhtcode@gmail.com>
2024-04-10 23:53:37 +0600

    10 APR 2024

--- module_14.5/project/settings.py
+++ module_14.5/project/settings.py
@@ -37,12 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms',
-    'crispy_tailwind',
 ]
 
-CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
-CRISPY_TEMPLATE_PACK = "tailwind"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -121,6 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- module_14.5/project/views.py
+++ module_14.5/project/views.py
@@ -4,9 +4,50 @@ from django import forms
 class UserForm(forms.Form):
     name = forms.CharField(
         label="Full Name",
-        max_length="150",
-        widget=(forms.TextInput(attrs={"class": "max-w-sm mx-auto"}))
+        initial="Your Name Here",
+        widget=forms.TextInput()
     )
+    
+    email = forms.EmailField()
+    
+    birth_date = forms.DateField(
+        label="Birth Date",
+        widget=forms.DateInput(
+            attrs={
+                "type": "date"
+                }
+            )
+    )
+
+    join_year_choices = ["2020", "2021", "2022", "2023", "2024"]
+    join_date = forms.DateField(
+        label="Joining Date",
+        widget=forms.SelectDateWidget(
+            years=join_year_choices,
+        )
+    )
+
+    weight = forms.DecimalField()
+
+    location_choices = [("UT", "Uttara"), ("BN", "Banani"), ("DH", "Dhanmondi"), ("GL", "Gulshan")]
+    location = forms.ChoiceField(
+        choices=location_choices
+    )
+
+    prefered_days_choices = [("S", "Sunday"), ("M", "Monday"), ("T", "Tuesday"), ("W", "Wednesday"), ("R", "Thursday")]
+    prefered_days = forms.MultipleChoiceField(
+        choices=prefered_days_choices
+    )
+
+    comment = forms.CharField(
+        widget=forms.Textarea(
+            attrs={"rows": 5}
+        )
+    )
+
+    agree = forms.BooleanField()
+
+
 
 def home(request):
     return render(request, "project/layout.html")
