30f1cda2a405bf2540d20982cadb9a0f32e0bdfa
Roufujjaman <rrhtcode@gmail.com>
2024-05-04 15:48:51 +0600

    04 04 24

--- module_16/tasks/urls.py
+++ module_16/tasks/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('add_task/', views.add_task, name='add_task'),
-    path('show_tasks/', views.show_tasks, name='show_tasks') 
+    path('show_tasks/', views.show_tasks, name='show_tasks'),
+    path('edit_task/<int:id>/', views.edit_task, name='edit_task'),
+    path('delete_task/<int:id>/', views.delet_task, name='delete_task')
 ]
--- module_16/tasks/views.py
+++ module_16/tasks/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from tasks.forms import TaskForm
+from tasks.models import Task
 
 # Create your views here.
 def add_task(request):
@@ -7,7 +8,7 @@ def add_task(request):
         task_form = TaskForm(request.POST)
         if task_form.is_valid():
             task_form.save()
-            return redirect('add_task')
+            return redirect('show_tasks')
         else:
             return render(request, 'tasks/add_task.html', {
                 'form': task_form
@@ -17,6 +18,29 @@ def add_task(request):
         'form': TaskForm()
     })
 
+def edit_task(request, id):
+    task = Task.objects.get(pk=id)
+    task_form = TaskForm(instance=task)
+    if request.method == 'POST':
+        task_form = TaskForm(request.POST, instance=task)
+        if task_form.is_valid():
+            task_form.save()
+            return redirect('show_tasks')
+        else:
+            return render(request, 'tasks/add_task.html', {
+                'form': task_form
+            })
+    return render(request, 'tasks/add_task.html', {
+        'form': task_form
+    })
+
+def delet_task(request, id):
+    task = Task.objects.get(pk=id)
+    task.delete()
+    return redirect('show_tasks')
 
 def show_tasks(request):
-    return render(request, 'tasks/show_tasks.html')
+    all_tasks = Task.objects.all()
+    return render(request, 'tasks/show_tasks.html', {
+        "tasks": all_tasks
+    })
