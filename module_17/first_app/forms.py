f8ed9c36276432e23877f4fe56f879877e7b5aa6
Roufujjaman <rrhtcode@gmail.com>
2024-05-07 23:25:53 +0600

    07 04 24

--- module_17/first_app/forms.py
+++ module_17/first_app/forms.py
@@ -1,7 +1,16 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
 
 class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(
+        required=True,
+        widget=forms.TextInput()
+    )
+    last_name = forms.CharField(
+        required=True,
+        widget=forms.TextInput()
+    )
     class Meta():
         model = User
         fields = ['username', 'first_name', 'last_name', 'email']
--- module_17/first_app/views.py
+++ module_17/first_app/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from .forms import RegisterForm
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     if request.method == 'POST':
         register_form = RegisterForm(request.POST)
         if register_form.is_valid():
-            register_form.save(commit=False)
+            # register_form.save(commit=False)
+            messages.success(request, 'Account created successfully')
+            register_form.save()
         else:
             return render(request, 'first_app/home.html', {
                 'form': register_form
