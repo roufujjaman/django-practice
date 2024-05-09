4faf26ccd327d4bdf7fbf71decdd6ba4b414b61f
Roufujjaman <rrhtcode@gmail.com>
2024-05-09 23:54:59 +0600

    09 05 24

--- module_17/first_app/views.py
+++ module_17/first_app/views.py
@@ -1,20 +1,4 @@
 from django.shortcuts import render
-from .forms import RegisterForm
-from django.contrib import messages
 
-# Create your views here.
-def home(request):
-    if request.method == 'POST':
-        register_form = RegisterForm(request.POST)
-        if register_form.is_valid():
-            # register_form.save(commit=False)
-            messages.success(request, 'Account created successfully')
-            register_form.save()
-        else:
-            return render(request, 'first_app/home.html', {
-                'form': register_form
-            })
-        
-    return render(request, 'first_app/home.html', {
-        'form': RegisterForm()
-    })
+def signup(request):
+    return render(request, 'first_app/signup.html')
