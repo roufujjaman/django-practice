1b319eac8e7c4dc21e85ae254ce1057e4d4c3939
Roufujjaman <rrhtcode@gmail.com>
2024-05-10 23:54:02 +0600

    10 05 24

--- module_17/first_app/urls.py
+++ module_17/first_app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/', views.signup, name='signup'),
-    
+    path('signup/', views.signup, name='register_user'),
+    path('signin/', views.signin, name='login_user'),
+    path('signout/', views.signout, name='logout_user'),
+    path('profile', views.profile, name='profile')
 ]
--- module_17/first_app/views.py
+++ module_17/first_app/views.py
@@ -1,4 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from . import forms
 
 def signup(request):
-    return render(request, 'first_app/signup.html')
+    if request.method == 'POST':
+        register_form = forms.RegisterFrom(request.POST)
+        if register_form.is_valid():
+            messages.success(request, 'Account Created Successfully')
+            register_form.save(commit=True)
+        else:
+            return render(request, 'first_app/signup.html', {
+                'form': register_form
+            })        
+    return render(request, 'first_app/signup.html', {
+        'form': forms.RegisterFrom()
+    })
+
+def signin(request):
+    if request.method == 'POST':
+        login_form = AuthenticationForm(request=request, data=request.POST)
+        if login_form.is_valid():
+            user_name = login_form.cleaned_data['username']
+            user_password = login_form.cleaned_data['password']
+            print(user_password)
+            user = authenticate(username=user_name, password=user_password)
+            if user is not None:
+                login(request, user)
+                return redirect('profile')
+        else:
+            return render()
+
+    return render(request, 'first_app/signin.html', {
+        'form': AuthenticationForm()
+    })
+def signout(request):
+    logout(request)
+    return redirect('login_user')
+
+def profile(request):
+    if request.user.is_authenticated:
+        return render(request, 'first_app/profile.html', {
+            'user': request.user
+        })
+    return redirect('login_user')
