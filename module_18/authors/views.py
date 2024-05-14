3e5f7c8176d6ce68754f04cc32495c4f6e19ee8f
Roufujjaman <rrhtcode@gmail.com>
2024-05-14 23:49:32 +0600

    14 05 24

--- module_18/authors/views.py
+++ module_18/authors/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from .forms import AuthorRegistrationForm
+from django.contrib.auth import authenticate
 
 # Create your views here.
 
 def register_user(request):
+
+    registration_form = AuthorRegistrationForm()
+    return render(request, 'author_forms.html', {
+        'form': registration_form,
+        'title': 'Registration Author'
+    })
+
+def login_user(request):
     pass
