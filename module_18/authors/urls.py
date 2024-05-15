eddb0f4d677399e955a9fb114acf94f0e97c2dc0
Roufujjaman <rrhtcode@gmail.com>
2024-05-15 23:09:06 +0600

    15 05 24

--- module_18/authors/urls.py
+++ module_18/authors/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = 'authors'
 urlpatterns = [
-    path('register', views.register_user, name='register_user'),
+    path('register', views.register_user, name='register_author'),
+    path('login', views.login_user, name='login_author')
     
 ]
--- module_18/authors/views.py
+++ module_18/authors/views.py
@@ -1,16 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import AuthorRegistrationForm
-from django.contrib.auth import authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 # Create your views here.
 
 def register_user(request):
+    if request.method == 'POST':
+        registration_form = AuthorRegistrationForm(request.POST)
+        if registration_form.is_valid():
+            registration_form.save()
+            messages.success(request, 'Account Created Successfully')
+            return redirect('home')
+    else:
+        registration_form = AuthorRegistrationForm()
 
-    registration_form = AuthorRegistrationForm()
     return render(request, 'author_forms.html', {
         'form': registration_form,
-        'title': 'Registration Author'
+        'title': 'Author Registration'
     })
 
 def login_user(request):
-    pass
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user_name = form.cleaned_data['username']
+            user_pass = form.cleaned_data['password']
+            user = authenticate(username=user_name, password=user_pass)
+            if user is not None:
+                login(request, user)
+                print('logged in')
+                messages.success(request, 'Logged In Successfully')
+                return redirect('home')
+    else:
+        form = AuthenticationForm()
+    
+    return render(request, 'author_forms.html', {
+        'form': form,
+        'title': 'Author Login'
+    })
+                
