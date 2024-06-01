97dac52e478208c9562ce942dd6e8691bc58bf9f
Roufujjaman <rrhtcode@gmail.com>
2024-06-01 17:20:45 +0600

    01 06 24

--- module_19/author/urls.py
+++ module_19/author/urls.py
@@ -1,1 +1,7 @@
 from django.urls import path
+from . import views
+
+app_name = 'author'
+urlpatterns = [
+    path('', views.register_user, name='register_user')
+]
--- module_19/author/views.py
+++ module_19/author/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
 # Create your views here.
+def register_user(request):
+    return render(request, 'author/user_forms.html', {
+        'form': UserCreationForm()
+    })
