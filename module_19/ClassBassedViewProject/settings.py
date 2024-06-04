1cd7ad437becdd9c2e651655c6d765959838933e
Roufujjaman <rrhtcode@gmail.com>
2024-06-04 23:35:12 +0600

    04 06 24

--- module_19/ClassBassedViewProject/settings.py
+++ module_19/ClassBassedViewProject/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
 ]
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
-
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
@@ -124,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
