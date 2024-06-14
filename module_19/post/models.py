c4083a01dda44f296ae6e0ba7905fc914537f54f
Roufujjaman <rrhtcode@gmail.com>
2024-06-14 23:55:41 +0600

    14 06 24

--- module_19/post/models.py
+++ module_19/post/models.py
@@ -1,7 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(
         max_length=150,
+    )
+    slug = models.SlugField()
+
+
+class Post(models.Model):
+    title = models.CharField()
+    author = models.ForeignKey(
+        User
+    )
+    content = models.TextField()
+    category = models.ForeignKey(
+        Category
     )
