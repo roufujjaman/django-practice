2eec6d816175636226a87ca10ec07d466e4de5d3
Roufujjaman <rrhtcode@gmail.com>
2024-07-14 23:35:50 +0600

    14 07 24

--- PracticeApp/problems/models.py
+++ PracticeApp/problems/models.py
@@ -2,7 +2,14 @@ from django.db import models
 
 # Create your models here.
 
-class Problems(models.Model):
+class Platform(models.Model):
+    name = models.CharField(max_length=150)
+
+class Problem(models.Model):
     titile = models.CharField(max_length=300)
-    platform = models.ForeignKey()
+    platform = models.ForeignKey(
+        Platform,
+        on_delete=models.CASCADE
+    )
     url = models.URLField()
+    series = models.IntegerField()
