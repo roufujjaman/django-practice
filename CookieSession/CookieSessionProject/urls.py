485f1cb872b15387e621fef7f0477eee9d2df0c9
Roufujjaman <rrhtcode@gmail.com>
2024-09-15 00:06:15 +0600

    15 09 24

--- CookieSession/CookieSessionProject/urls.py
+++ CookieSession/CookieSessionProject/urls.py
@@ -20,8 +20,12 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('set_cookie/<str:ckey>/<str:cval>/<int:days>', views.set_cookie, {'days': 7}, name='set_cookie'),
+    path('set_cookie/<str:ckey>/<str:cval>/', views.set_cookie, name='set_cookie'),
+    path('set_cookie/<str:ckey>/<str:cval>/<int:days>/', views.set_cookie, name='set_cookie_days'),
     path('get_cookie/', views.get_cookie, name='get_cookie'),
     path('del_cookie/<str:name>', views.delete_cookie, name='delete_cookie'),
-    path('default/<str:name>/', views.default)
+    path('default/<str:name>/', views.default),
+    path('set_session/<str:username>/<str:language>/', views.set_session, name='set_session'),
+    path('get_session/', views.get_session, name='get_session'),
+    path('del_session/', views.delete_session, name='delete_session')
 ]
--- CookieSession/CookieSessionProject/views.py
+++ CookieSession/CookieSessionProject/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
+from datetime import datetime, timedelta
 
-
-def set_cookie(request, ckey, cval, days=3):
-    response = render(request, 'cookie.html', {
-        'action': 'SETTING COOKIE [set_cookie]'
-    })
-    response.set_cookie(ckey, cval)
-    return response
+def set_cookie(request, ckey, cval, days=None):
+    if days == None:
+        response = render(request, 'cookie.html', {
+            'action': 'SETTING COOKIE [set_cookie]'
+        })
+        response.set_cookie(ckey, cval)
+        return response
+    else:
+        response = render(request, 'cookie.html', {
+            'action': f'SETTING COOKIE FOR {days} DAYS [set_cookie_days]'
+        })
+        response.set_cookie(ckey, cval, expires=datetime.now()+timedelta(days=days))
+        return response
 
 def get_cookie(request):
     cookies = request.COOKIES
@@ -26,4 +33,27 @@ def delete_cookie(request, name):
 def default(request, name='roufujjaman'):
     return render(request, 'cookie.html', {
         'action': name
+    })
+
+def set_session(request, username, language):
+    data = {
+        'username': username,
+        'lanuage': language
+    }
+    request.session.update(data)
+    return render(request, 'session.html', {
+        'action': 'SESSION CREATED'
+    })
+
+def get_session(request):
+    data = request.session.items
+    return render(request, 'session.html', {
+        'action': 'SESSION DATA',
+        'session': data
+    })
+
+def delete_session(request):
+    request.session.flush()
+    return render(request, 'session.html', {
+        'action': 'SESSION FLUSHED'
     })
