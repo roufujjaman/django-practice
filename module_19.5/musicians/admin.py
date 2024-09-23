873abb32701e8a7ec6c54f261bc243b9d4527431
Roufujjaman <rrhtcode@gmail.com>
2024-09-23 23:32:41 +0600

    23 09 24

--- module_19.5/musicians/admin.py
+++ module_19.5/musicians/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+class MusiciansAdmin(admin.ModelAdmin):
+    list_display = ('fname', 'lname', 'email')
+    list_display_links = ('email',)
+admin.site.register(models.Musicians, MusiciansAdmin)
--- module_19.5/musicians/models.py
+++ module_19.5/musicians/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-from django.contrib.admin import models
 
 class Musicians(models.Model):
-    fname = models.Ch
-, max_age=60
+    fname = models.CharField(max_length=25)
+    lname = models.CharField(max_length=25)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=15)
+    instrument = models.CharField(max_length=50)
