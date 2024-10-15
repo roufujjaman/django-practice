d968e3a8439d0aa8986ca854cceb61bc77a57402
Roufujjaman <rrhtcode@gmail.com>
2024-10-15 23:00:32 +0600

    15 10 24

--- smartnotes/notes/views.py
+++ smartnotes/notes/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
 from .models import Notes 
+from django.views.generic import ListView, DetailView
 # Create your views here.
 
-def list(request):
-    all_notes = Notes.objects.all()
-    return render(request, 'notes/notes_list.html', {
-        "notes": all_notes
-    })
+
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = "notes"
+
+
+# def list(request):
+#     all_notes = Notes.objects.all()
+#     return render(request, 'notes/notes_list.html', {
+#         "notes": all_notes
+#     })
+
+class NotesDetailView(DetailView):
+    model = Notes 
+    context_object_name = "note"
+
+
+# def detail(request, pk):
+#     try:
+#         note = Notes.objects.get(pk=pk)
+#     except Notes.DoesNotExist:
+#         raise Http404("Note Does Not Exist")
+#     return render(request, 'notes/notes_detail.html', {
+#         "note": note
+#     })
